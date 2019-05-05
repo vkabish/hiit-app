7c1f4d0bc910c1f46fac36c8cb6515ce1578aca3
Volodymyr Kabysh <v.kabish@ukr.net>
2019-05-05 19:21:35 +0300

    Updates

---
+++ precache-manifest.bb8c16e8087dbf1e7fc6c41d5ca8b26d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c2dcb7afb0e81e16dba",
+    "url": "/hiit-app/static/js/runtime~main.8c8ccb99.js"
+  },
+  {
+    "revision": "fd6e0f0b60b8b9657e07",
+    "url": "/hiit-app/static/js/main.8b021378.chunk.js"
+  },
+  {
+    "revision": "1f12e7e2e4f9d122b0c6",
+    "url": "/hiit-app/static/js/2.07385925.chunk.js"
+  },
+  {
+    "revision": "fd6e0f0b60b8b9657e07",
+    "url": "/hiit-app/static/css/main.623fb0a0.chunk.css"
+  },
+  {
+    "revision": "03c398678eb567f709733382addf9c21",
+    "url": "/hiit-app/index.html"
+  }
+];
